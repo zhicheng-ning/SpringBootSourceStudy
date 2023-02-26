7f7d29f3da074f8d6ff8ea6856691c5736611ad6
nzcer <ningzhicheng1215@qq.com>
2023-02-26 11:22:36 +0800

    feat: initial spring-boot source study project

---
+++ src/main/java/cn/nzcer/springbootsourcestudy/SpringBootSourceStudyApplication.java
@@ -0,0 +1,13 @@
+package cn.nzcer.springbootsourcestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSourceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSourceStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/nzcer/springbootsourcestudy/SpringBootSourceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nzcer.springbootsourcestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSourceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
